4ac3947263836a9585884aad82239f113dcbac4d
William Brockhus <pickeringw@gmail.com>
2021-01-27 11:05:54 +1100

    add getScopedCredentialName

---
+++ vars/getScopedCredentialName.groovy
@@ -0,0 +1,34 @@
+import com.cloudbees.plugins.credentials.CredentialsProvider
+import com.cloudbees.plugins.credentials.common.StandardCredentials
+import com.cloudbees.plugins.credentials.domains.DomainRequirement
+
+Boolean credentialExists(String id) {
+	def cred = CredentialsProvider.findCredentialById(
+		id,
+		StandardCredentials.class,
+		currentBuild.getRawBuild(),
+		Collections.<DomainRequirement>emptyList()
+	)
+
+	return cred != null
+}
+
+/**
+ * Check if credential exists.
+ * @param id credentialsId to check
+ * @return True if a credential exists and is accessible, false otherwise.
+ */
+String call(String id) {
+	globalId = id.toUpperCase().toString()
+	branchId = "${globalId}_${BRANCH_NAME}".toUpperCase().toString()
+
+	if (credentialExists(branchId)) {
+		return branchId
+	}
+
+	if (credentialExists(globalId)) {
+		return globalId
+	}
+
+	return null
+}
