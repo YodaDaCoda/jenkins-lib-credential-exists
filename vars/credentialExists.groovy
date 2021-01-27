fa919b4a21bdaeea41fad28faf1a604e07bdc9e1
William Brockhus <pickeringw@gmail.com>
2021-01-27 11:03:22 +1100

    handle branch-scoped credential

--- vars/credentialExists.groovy
+++ vars/credentialExists.groovy
@@ -2,17 +2,33 @@ import com.cloudbees.plugins.credentials.CredentialsProvider
 import com.cloudbees.plugins.credentials.common.StandardCredentials
 import com.cloudbees.plugins.credentials.domains.DomainRequirement
 
+Boolean credentialExists(String id) {
+	def cred = CredentialsProvider.findCredentialById(
+		id,
+		StandardCredentials.class,
+		currentBuild.getRawBuild(),
+		Collections.<DomainRequirement>emptyList()
+	)
+
+	return cred != null
+}
+
 /**
  * Check if credential exists.
  * @param id credentialsId to check
  * @return True if a credential exists and is accessible, false otherwise.
  */
 boolean call(String id) {
-  def available_credentials = CredentialsProvider.findCredentialById(
-    id,
-    StandardCredentials.class,
-    currentBuild.getRawBuild(),
-    Collections.<DomainRequirement>emptyList()
-  )
-  return available_credentials != null
+	globalId = id.toUpperCase().toString()
+	branchId = "${globalId}_${BRANCH_NAME}".toUpperCase().toString()
+
+	if (credentialExists(branchId)) {
+		return true
+	}
+
+	if (credentialExists(globalId)) {
+		return true
+	}
+
+	return false
 }
