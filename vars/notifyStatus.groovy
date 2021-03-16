8f056bea51fca867276c483a2f5aca403757949b
William Brockhus <pickeringw+github@gmail.com>
2021-03-16 21:49:30 +1100

    fix(notify): += operator doesn't exist for string?

--- vars/notifyStatus.groovy
+++ vars/notifyStatus.groovy
@@ -26,13 +26,13 @@ String getMessage() {
 	String ret = "${status}: ${currentBuild.fullDisplayName}"
 
 	if (env.CHANGE_ID != null) {
-		ret += " (<${env.CHANGE_URL}|${env.CHANGE_ID}> by ${env.CHANGE_AUTHOR_DISPLAY_NAME})"
+		ret = "${ret} (<${env.CHANGE_URL}|${env.CHANGE_ID}> by ${env.CHANGE_AUTHOR_DISPLAY_NAME})"
 
 	} else if (env.BRANCH_NAME != null) {
-		ret += " (branch: ${env.BRANCH_NAME})"
+		ret = "${ret} (branch: ${env.BRANCH_NAME})"
 	}
 
-	ret += " after ${currentBuild.durationString} (<${RUN_DISPLAY_URL}|Open>)"
+	ret = "${ret} after ${currentBuild.durationString} (<${RUN_DISPLAY_URL}|Open>)"
 
 	return ret
 }
