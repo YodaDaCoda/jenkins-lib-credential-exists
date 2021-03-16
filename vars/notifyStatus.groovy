d87937ace0374518bf81d87fac4c4c699804f3fd
William Brockhus <pickeringw+github@gmail.com>
2021-03-16 22:00:12 +1100

    test

--- vars/notifyStatus.groovy
+++ vars/notifyStatus.groovy
@@ -22,24 +22,44 @@ String getStatusColor(String status) {
 }
 
 String getMessage() {
+	out.println('begin getMessage()')
 
 	String ret = "${status}: ${currentBuild.fullDisplayName}"
 
+	out.println(ret)
+	out.println("$(env.CHANGE_ID}")
+	out.println("$(env.BRANCH_NAME}")
+	out.println(env.CHANGE_ID)
+	out.println(env.BRANCH_NAME)
+	out.println(env)
+
 	if ("${env.CHANGE_ID}" != null) {
 		ret = "${ret} (<${env.CHANGE_URL}|${env.CHANGE_ID}> by ${env.CHANGE_AUTHOR_DISPLAY_NAME})"
-
+		out.println('change_id not null')
+		out.println(ret)
 	} else if ("${env.BRANCH_NAME}" != null) {
+		out.println('branch_name not null')
+		out.println(ret)
 		ret = "${ret} (branch: ${env.BRANCH_NAME})"
 	}
 
+	out.println(ret)
+
 	ret = "${ret} after ${currentBuild.durationString} (<${RUN_DISPLAY_URL}|Open>)"
+
+	out.println(ret)
+
+	out.println('end getMessage()')
 
 	return ret
 }
 
 void call(String status) {
+	out.println('notifyStatus')
 	String color = getStatusColor(status)
+	out.println(color)
 	String message = getMessage()
+	out.println(message)
 	slackSend (
 		channel : '#jenkins-ci',
 		color   : color,
